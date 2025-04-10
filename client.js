26f49bd672e40c375ced3f69e92d8eb68f858aa1
arthursereno <arthurbarbosa2809@gmail.com>
2025-04-10 14:29:07 -0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+window.TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      text: '📌 Ativar Pixel',
+      callback: function(t) {
+        return t.card('id')
+          .then(function(card) {
+            const webhookUrl = `https://n8n.api-blogfeelgood.fun/webhook-test/ativar-pixel?cardId=${card.id}`;
+            return fetch(webhookUrl, {
+              method: 'GET'
+            })
+            .then(() => t.alert({ message: '✅ Pixel Ativo!' }))
+            .catch(() => t.alert({ message: '❌ Erro ao ativar o Pixel.' }));
+          });
+      }
+    }];
+  }
+});
